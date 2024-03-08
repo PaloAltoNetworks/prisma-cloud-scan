cb6b72c1214ba97b2dc766ec86892f1bfd62cb55
Stephen Gordon <sgordon@paloaltonetworks.com>
2024-03-08 10:03:36 -0600

    add support for unreviewed sev

--- dist/index.js
+++ dist/index.js
@@ -14148,6 +14148,7 @@ function convertPrismaSeverity(severity) {
       return "none";
     case "unimportant":
     case "negligible":
+    case "unreviewed":  
       return "none";
     default:
       throw new Error(`Unknown severity: ${severity}`);
