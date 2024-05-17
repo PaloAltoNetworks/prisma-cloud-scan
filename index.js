1c6de772257f85a3ac8e5cdaccc4a03457e8094d
sgordon46 <sgordon@paloaltonetworks.com>
2024-05-17 08:58:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -194,8 +194,8 @@ function convertPrismaSeverity(severity) {
       return "none";
     case "unimportant":
     case "negligible":
-    case "unreviewed":  
-    case "unassigned":  
+    case "unreviewed":
+    case "unassigned": 
       return "none";
     default:
       throw new Error(`Unknown severity: ${severity}`);
